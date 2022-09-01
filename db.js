d27fa97b63de8557fa5b2d565e4dabbdf56254fc
Saifur Rahman <saifur.dohs@gmail.com>
2022-09-01 07:58:26 +0600

    Videos and Tags Database added

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const todos = require("./data/todos");
+const videos = require("./data/videos");
+const tags = require("./data/tags");
 
 module.exports = {
-  todos
+  todos,
+  videos,
+  tags
 }
 
