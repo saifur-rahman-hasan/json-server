eaf9d85178a4e03a8beb7350380c0a9553d292b8
Saifur Rahman <saifur.dohs@gmail.com>
2022-09-06 08:52:03 +0600

    Transactions database added

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const todos = require("./data/todos");
 const videos = require("./data/videos");
 const tags = require("./data/tags");
+const transactions = require("./data/transactions");
 
 module.exports = {
   todos,
   videos,
-  tags
+  tags,
+  transactions
 }
 
